e99b88a8b46ba49f22de428a725973d4f2712f78
xabi1000 <babelwado@gmail.com>
2022-11-05 19:37:06 +0100

    astro  & scss components

---
+++ src/js/dark_mode.js
@@ -0,0 +1,18 @@
+
+const btn = document.querySelector('.btn');
+let theme = 'light';
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme == 'dark') document.body.classList.add('dark-theme');
+
+const darkMode = () => {
+  console.log([...document.getElementsByClassName('light-theme')])
+  document.body.classList.toggle('dark-theme');
+  if (document.body.classList.contains('dark-theme')) {
+    theme = 'dark';
+    localStorage.setItem('theme', theme);
+  }
+}
+
+btn.addEventListener('click', darkMode);
+
